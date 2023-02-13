b4234a05742ba98f6c4f66a03343aa1a0e79d3a4
shashavalishaik <114146351+shashavalishaik@users.noreply.github.com>
2023-02-13 23:28:41 +0530

    Add files via upload

---
+++ spring_boot_selenium_latest_feature/src/main/java/com/spring/selenium/latest/feature/SpringBootSeleniumExtentreport1Application.java
@@ -0,0 +1,13 @@
+package com.spring.selenium.latest.feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeleniumExtentreport1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeleniumExtentreport1Application.class, args);
+	}
+
+}
---
+++ spring_boot_selenium_latest_feature/src/test/java/com/spring/selenium/latest/feature/SpringBootSeleniumExtentreport1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.selenium.latest.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSeleniumExtentreport1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
