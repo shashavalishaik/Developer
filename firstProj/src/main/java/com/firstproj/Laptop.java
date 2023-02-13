4a2a25f6d24cfcd74769f1ec514f77f16136e001
shashavalishaik <114146351+shashavalishaik@users.noreply.github.com>
2023-02-13 23:10:48 +0530

    Add files via upload

---
+++ firstProj/src/main/java/com/firstproj/Laptop.java
@@ -0,0 +1,13 @@
+package com.firstproj;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	
+	public void compile()
+	{
+		System.out.println("Compling");
+	}
+
+}
